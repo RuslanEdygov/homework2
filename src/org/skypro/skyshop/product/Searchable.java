761f12e6bc87dac8357ef2f04bfdfdd512afc4b5
Admin <ruslanedygov@gmail.com>
2025-05-03 12:00:03 +0300

    dz15 ООП. Полиморфизм. Интерфейсы №1

---
+++ src/org/skypro/skyshop/product/Searchable.java
@@ -0,0 +1,16 @@
+package org.skypro.skyshop.product;
+
+public interface Searchable {
+    String getSearchTerm();
+    String getContentType();
+    String getName();
+    default String getStringRepresentation() {
+        return getName() + " — тип " + getContentType();
+    }
+
+    //String getSearchTerm();
+
+    //String getContentType();
+
+    //String getName();
+}
---
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -0,0 +1,35 @@
+package org.skypro.skyshop.search;
+
+import org.skypro.skyshop.product.Searchable;
+
+public class SearchEngine {
+    private final Searchable[] data;
+    private int size = 0;
+
+    public SearchEngine(int capacity) {
+        this.data = new Searchable[capacity];
+    }
+
+    public void add(Searchable item) {
+        if (size < data.length) {
+            data[size++] = item;
+        } else {
+            System.out.println("Поисковой индекс переполнен, элемент не добавлен: " +
+                    item.getName());
+        }
+    }
+
+    public Searchable[] search(String query) {
+        Searchable[] results = new Searchable[5];
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (data[i].getSearchTerm().toLowerCase().contains(query.toLowerCase())) {
+                results[count++] = data[i];
+                if (count == 5) break;
+            }
+        }
+        return results;
+    }
+}
+
